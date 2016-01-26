0cf1a9bd6fa4d28b0a695380ca0bffc730c851dc
Ferencz Magyari <afmagyari@gmail.com>
2016-01-26 11:16:08 -0800

    initial commit

---
+++ api-router.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var apiRouter = express.Router();
+var urlBase = '/api/customers';
+
+app.use(urlBase, apiRouter);
+
+apiRouter.get(urlBase, function(req, res) {
+    // find all customers 
+});
+
+apiRouter.get(urlBase + '/:id', function(req, res) {
+    // find customer by id
+});
+
+apiRouter.post(urlBase, function(req, res) {
+    // save new customer
+});
+
+apiRouter.put(urlBase + '/:id', function(req, res) {
+   // find customer by id and update
+});
+
+apiRouter.delete(urlBase + '/:id', function(req, res) {
+   // find customer by id and delete
+});
+
+apiRouter.get(urlBase + '/:id/orders', function(req, res) {
+   // find customer by id and return customer.orders 
+});
